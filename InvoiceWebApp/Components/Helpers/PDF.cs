adf2ded9f94a81e6691adc3af47e8cd80779c8d5
Johan Bos <bosbosjohan@gmail.com>
2018-08-01 18:29:49 +0200

    - Working on pdf contents

--- InvoiceWebApp/Components/Helpers/PDF.cs
+++ InvoiceWebApp/Components/Helpers/PDF.cs
@@ -66,14 +66,35 @@ namespace InvoiceWebApp.Components.Helpers
 
             //--------------------- CONTENT -----------------------------// 595 x 842
 
-            BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            BaseFont helvetica = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
             BaseFont title = BaseFont.CreateFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
 
+            // Company Name
             cb.BeginText();
             cb.SetFontAndSize(title, 26);
 
-            cb.SetTextMatrix(380, 800);
+            cb.SetTextMatrix(30, 800);
             cb.ShowText(this.settings.CompanyName);
+
+            cb.EndText();
+
+            // Company Info Line 1
+            cb.SetLineWidth(1);
+            cb.SetRgbColorStroke(0, 100, 240);
+            cb.Rectangle(330, 810, 75, 1);
+            cb.Rectangle(430, 810, 150, 1);
+            cb.Stroke();
+
+            // Company Info
+            cb.BeginText();
+            cb.SetFontAndSize(helvetica, 15);
+
+            cb.SetTextMatrix(330, 785);
+            cb.ShowText("Address");
+            cb.SetTextMatrix(330, 770);
+            cb.ShowText("");
+            cb.SetTextMatrix(330, 755);
+            cb.ShowText("");
 
             cb.EndText();
 
@@ -86,7 +107,7 @@ namespace InvoiceWebApp.Components.Helpers
 
         private Document InitDocument()
         {
-            Document doc = new Document(PageSize.A4);
+            Document doc = new Document(PageSize.A4, 30, 30, 42, 42);
             doc.AddSubject(String.Format("This invoice belongs to {0}. {1}", debtor.FirstName[0], debtor.LastName));
             doc.AddKeywords("Invoice, Payment");
             doc.AddCreator(this.settings.CompanyName);
