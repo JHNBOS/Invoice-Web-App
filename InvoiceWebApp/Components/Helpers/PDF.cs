73d9ddffaefd0f5dc2a33e2126b1e6961afc0a2e
Johan Bos <bosbosjohan@gmail.com>
2018-08-07 16:03:44 +0200

    - Pdf code cleanup and added comments

--- InvoiceWebApp/Components/Helpers/PDF.cs
+++ InvoiceWebApp/Components/Helpers/PDF.cs
@@ -64,69 +64,31 @@ namespace InvoiceWebApp.Components.Helpers
             doc.Open();
             PdfContentByte cb = writer.DirectContent;
 
-            //--------------------- CONTENT -----------------------------// 595 x 842
-
+            // Fonts
             BaseFont helvetica = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
             BaseFont title = BaseFont.CreateFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
 
-            // Company Name
+            // Company name/logo
             cb.BeginText();
             cb.SetFontAndSize(title, 26);
-
             cb.SetTextMatrix(30, 785);
             cb.ShowText(this.settings.CompanyName);
-
             cb.EndText();
 
-            // Company Info Line 1
-            //cb.SetLineWidth(0.4f);
-            //cb.SetRgbColorStroke(0, 100, 240);
-
-            //cb.Rectangle(385, 810, 40, 0.4f);
-            //cb.Rectangle(437, 810, 118, 0.4f);
-
-            //cb.Rectangle(385, 680, 40, 0.4f);
-            //cb.Rectangle(437, 680, 118, 0.4f);
-
-            //cb.Stroke();
-
+            // Company info
             cb.BeginText();
             cb.SetFontAndSize(helvetica, 10f);
-
-            // -------------- Company Info ---------------------
-
-            // Address
-            cb.ShowTextAligned(Element.ALIGN_RIGHT, "", 420, 795, 0);
-            cb.ShowTextAligned(Element.ALIGN_RIGHT, "", 420, 780, 0);
             cb.ShowTextAligned(Element.ALIGN_LEFT, settings.Address, 430, 795, 0);
             cb.ShowTextAligned(Element.ALIGN_LEFT, settings.PostalCode + ", " + settings.City, 430, 780, 0);
-
-            // E-mail
-            cb.ShowTextAligned(Element.ALIGN_RIGHT, "", 430, 760, 0);
             cb.ShowTextAligned(Element.ALIGN_LEFT, settings.Email, 430, 760, 0);
-
-            // Phone
-            cb.ShowTextAligned(Element.ALIGN_RIGHT, "", 430, 745, 0);
             cb.ShowTextAligned(Element.ALIGN_LEFT, settings.Phone, 430, 745, 0);
-
-            // Business nr
-            //cb.ShowTextAligned(Element.ALIGN_RIGHT, "COC:", 420, 725, 0);
             cb.ShowTextAligned(Element.ALIGN_LEFT, "COC: " + settings.BusinessNumber, 430, 725, 0);
-
-            // VAT nr
-            //cb.ShowTextAligned(Element.ALIGN_RIGHT, "VAT:", 420, 710, 0);
             cb.ShowTextAligned(Element.ALIGN_LEFT, "VAT: " + settings.VAT, 430, 710, 0);
-
-            // Bank account
-            //cb.ShowTextAligned(Element.ALIGN_RIGHT, "IBAN:", 420, 695, 0);
             cb.ShowTextAligned(Element.ALIGN_LEFT, "IBAN: " + settings.BankAccount, 430, 695, 0);
-
             cb.EndText();
 
-            //--------------------- CONTENT -----------------------------//
-
+            // Close and return pdf
             doc.Close();
-
             return ms.ToArray();
         }
 
