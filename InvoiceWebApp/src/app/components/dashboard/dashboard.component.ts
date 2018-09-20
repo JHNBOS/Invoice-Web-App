64f0a51f9d7a223296854c4bcfc0762578f83a41
Johan Bos <bosbosjohan@gmail.com>
2018-09-20 10:15:07 +0200

    - Charts in dashboard component

--- InvoiceWebApp/src/app/components/dashboard/dashboard.component.ts
+++ InvoiceWebApp/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Chart, ChartConfiguration } from 'chart.js';
 
 import Settings from '../../shared/models/settings.model';
 import User from '../../shared/models/user.model';
@@ -11,10 +12,13 @@ import { UserService } from '../../shared/services/user.service';
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
     settings: Settings = JSON.parse(sessionStorage.getItem('settings'));
     currentUser: User = JSON.parse(sessionStorage.getItem('signedInUser'));
-    invoice_count = 0;
+    invoice_count: number[] = [];
+
+    chartOne: any = null;
+    chartTwo: any = null;
 
     constructor(private titleService: Title, private userService: UserService, private invoiceService: InvoiceService) { }
 
@@ -23,10 +27,83 @@ export class DashboardComponent implements OnInit {
         this.getUnpaidInvoiceCount();
     }
 
+    ngAfterViewInit() {
+    }
+
     getUnpaidInvoiceCount() {
         this.invoiceService.getAll().subscribe(
-            (response) => this.invoice_count = response.filter(f => f.is_paid === false).length,
+            (response) => {
+                this.invoice_count[0] = response.filter(f => f.is_paid === false).length;
+                this.invoice_count[1] = response.filter(f => f.is_paid === true).length;
+
+                setTimeout(() => { this.createChartOne(); this.createChartTwo(); }, 250);
+            },
             (error) => { throw error; }
         );
+    }
+
+    createChartOne() {
+        const canvas = <HTMLCanvasElement>document.getElementById('chart-one');
+        const ctx = canvas.getContext('2d');
+
+        this.chartOne = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Unpaid', 'Paid'],
+                datasets: [
+                    {
+                        data: [this.invoice_count[0], this.invoice_count[1]],
+                        backgroundColor: ['#f4d41f', '#12a4e8'],
+                        borderColor: ['#e8c620', '#1095d3'],
+                        fill: true
+                    },
+                ]
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    display: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: false
+                    }],
+                    yAxes: [{
+                        display: false
+                    }],
+                }
+            },
+        });
+    }
+
+    createChartTwo() {
+        const canvas = <HTMLCanvasElement>document.getElementById('chart-two');
+        const ctx = canvas.getContext('2d');
+
+        this.chartTwo = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Test', 'Test', 'Test', 'Test', 'Test', 'Test', 'Test'],
+                datasets: [
+                    {
+                        data: [0, 1, 2, 3, 4, 5, 6],
+                    },
+                ]
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    display: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true
+                    }],
+                    yAxes: [{
+                        display: true
+                    }],
+                }
+            },
+        });
     }
 }
