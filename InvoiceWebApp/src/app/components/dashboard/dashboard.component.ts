e78655c3c57637237b8131c8bd29223973f6ac4c
Johan Bos <bosbosjohan@gmail.com>
2018-09-22 14:15:07 +0200

    - Finished charts in dashboard.component

--- InvoiceWebApp/src/app/components/dashboard/dashboard.component.ts
+++ InvoiceWebApp/src/app/components/dashboard/dashboard.component.ts
@@ -15,7 +15,9 @@ import { UserService } from '../../shared/services/user.service';
 export class DashboardComponent implements OnInit, AfterViewInit {
     settings: Settings = JSON.parse(sessionStorage.getItem('settings'));
     currentUser: User = JSON.parse(sessionStorage.getItem('signedInUser'));
+
     invoice_count: number[] = [];
+    invoice_cash: number[] = [];
 
     chartOne: any = null;
     chartTwo: any = null;
@@ -35,6 +37,8 @@ export class DashboardComponent implements OnInit, AfterViewInit {
             (response) => {
                 this.invoice_count[0] = response.filter(f => f.is_paid === false).length;
                 this.invoice_count[1] = response.filter(f => f.is_paid === true).length;
+                this.invoice_cash[0] = response.reduce((a, b) => a + b.total, 0);
+                this.invoice_cash[1] = response.filter(f => f.is_paid === true).reduce((a, b) => a + b.total, 0);
 
                 setTimeout(() => { this.createChartOne(); this.createChartTwo(); }, 250);
             },
@@ -61,6 +65,11 @@ export class DashboardComponent implements OnInit, AfterViewInit {
             },
             options: {
                 responsive: true,
+                title: {
+                    display: true,
+                    text: 'Total Paid/Unpaid Invoices',
+                    position: 'bottom'
+                },
                 legend: {
                     display: true
                 },
@@ -81,29 +90,76 @@ export class DashboardComponent implements OnInit, AfterViewInit {
         const ctx = canvas.getContext('2d');
 
         this.chartTwo = new Chart(ctx, {
-            type: 'line',
+            type: 'bar',
             data: {
-                labels: ['Test', 'Test', 'Test', 'Test', 'Test', 'Test', 'Test'],
+                labels: ['Amount to be cashed', 'Currently cashed amount'],
                 datasets: [
                     {
-                        data: [0, 1, 2, 3, 4, 5, 6],
+                        label: 'Amount to be cashed',
+                        data: [this.invoice_cash[0]],
+                        backgroundColor: ['#f4d41f'],
+                        borderColor: ['#e8c620'],
+                        fill: true
+                    },
+                    {
+                        label: 'Currently cashed amount',
+                        data: [this.invoice_cash[1]],
+                        backgroundColor: ['#12a4e8'],
+                        borderColor: ['#1095d3'],
+                        fill: true
                     },
                 ]
             },
             options: {
                 responsive: true,
+                title: {
+                    display: true,
+                    text: 'Total Amount Cashed',
+                    position: 'bottom'
+                },
                 legend: {
                     display: true
                 },
                 scales: {
                     xAxes: [{
-                        display: true
+                        display: false,
+                        maxBarThickness: 100,
                     }],
                     yAxes: [{
-                        display: true
-                    }],
-                }
+                        ticks: {
+                            beginAtZero: true,
+                            callback: function (value, index, values) {
+                                if (parseInt(value, 2) >= 1000) {
+                                    return this.getLocaleString(value);
+                                } else {
+                                    return '€' + value;
+                                }
+                            }
+                        }
+                    }]
+                },
+                tooltips: {
+                    callbacks: {
+                        label: function (t, d) {
+                            if (t.datasetIndex === 0) {
+                                const xLabel = d.datasets[t.datasetIndex].label;
+                                // tslint:disable-next-line:max-line-length
+                                const yLabel = parseInt(t.yLabel, 2) >= 1000 ? this.getLocaleString(t.yLabel) : '€' + t.yLabel;
+                                return xLabel + ': ' + yLabel;
+                            } else if (t.datasetIndex === 1) {
+                                const xLabel = d.datasets[t.datasetIndex].label;
+                                // tslint:disable-next-line:max-line-length
+                                const yLabel = parseInt(t.yLabel, 2) >= 1000 ? this.getLocaleString(t.yLabel) : '€' + t.yLabel;
+                                return xLabel + ': ' + yLabel;
+                            }
+                        }
+                    }
+                },
             },
         });
+    }
+
+    getLocaleString(total: number): string {
+        return total.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' });
     }
 }
